81395e01cc51596078f771f71c1aa9c96dc8993a
cjneville <7499364+cjneville@users.noreply.github.com>
2021-11-16 20:57:19 +0000

    added loop

--- controllers/userController.js
+++ controllers/userController.js
@@ -76,9 +76,6 @@ async function matchNames(names)  {
 
 const user_assign = (req, res) => {
   var array = [];
-  for (var i = 0; i < 10; i++) {
-
-
   User.find().sort({ createdAt: -1 })
     .then(function(result) {
             matchNames(result).then(() => {res.redirect('/users')});
@@ -86,7 +83,7 @@ const user_assign = (req, res) => {
     .catch(err => {
       console.log(err);
     });
-  }
+
 //   const user = new User(req.body);
 //   user.save()
 //     .then(result => {
